8ce4ac6f045ff22311f160221d98a41f120d4b5b
1zumisawashun <shun.1zumisawa@gmail.com>
2025-09-14 11:13:40 +0900

    fix

--- src/utils/prisma/seed.ts
+++ src/utils/prisma/seed.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import { mockArticles, mockCategories, mockUsers } from "./mocks";
-
-const prisma = new PrismaClient();
+import { prisma } from "./prismaClient";
 
 async function main() {
 	// delete all
